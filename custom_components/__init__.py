6170d2dd98e55c285368ae01d0802609c2aa9760
James <37006972+sam43434@users.noreply.github.com>
2022-11-15 16:33:45 +1030

    Add files via upload

---
+++ custom_components/__init__.py
@@ -0,0 +1,77 @@
+"""The Remootio integration."""
+from __future__ import annotations
+
+import logging
+
+from aioremootio import ConnectionOptions, RemootioClient
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import CONF_HOST, Platform
+from homeassistant.core import HomeAssistant
+
+from .const import (
+    CONF_API_AUTH_KEY,
+    CONF_API_SECRET_KEY,
+    CONF_SERIAL_NUMBER,
+    DOMAIN,
+    REMOOTIO_CLIENT,
+)
+from .utils import create_client
+
+_LOGGER = logging.getLogger(__name__)
+
+PLATFORMS = [Platform.COVER]
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up Remootio from a config entry."""
+
+    _LOGGER.debug("Doing async_setup_entry. entry [%s]", entry.as_dict())
+
+    connection_options: ConnectionOptions = ConnectionOptions(
+        entry.data[CONF_HOST],
+        entry.data[CONF_API_SECRET_KEY],
+        entry.data[CONF_API_AUTH_KEY],
+    )
+    serial_number: str = entry.data[CONF_SERIAL_NUMBER]
+
+    remootio_client: RemootioClient = await create_client(
+        hass, connection_options, _LOGGER, serial_number
+    )
+
+    hass_data = hass.data.setdefault(DOMAIN, {}).setdefault(entry.entry_id, {})
+    hass_data[REMOOTIO_CLIENT] = remootio_client
+
+    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+
+    _LOGGER.debug(
+        "Doing async_unload_entry. entry [%s] hass.data[%s][%s] [%s]",
+        entry.as_dict(),
+        DOMAIN,
+        entry.entry_id,
+        hass.data.get(DOMAIN, {}).get(entry.entry_id, {}),
+    )
+
+    platforms_unloaded = await hass.config_entries.async_unload_platforms(
+        entry, PLATFORMS
+    )
+
+    if platforms_unloaded and DOMAIN in hass.data.keys():
+        hass_data = hass.data[DOMAIN].pop(entry.entry_id, {})
+        if REMOOTIO_CLIENT in hass_data:
+            remootio_client: RemootioClient = hass_data.pop(REMOOTIO_CLIENT, None)
+            if remootio_client is not None:
+                terminated: bool = await remootio_client.terminate()
+                if terminated:
+                    _LOGGER.debug(
+                        "Remootio client successfully terminated. entry [%s]",
+                        entry.as_dict(),
+                    )
+
+    return platforms_unloaded
---
+++ custom_components/const.py
@@ -0,0 +1,26 @@
+"""Constants for the Remootio integration."""
+
+# Domain of the integration
+DOMAIN = "remootio"
+
+# Timeout used in methods of remootio.utils
+REMOOTIO_TIMEOUT = 60
+
+# Deleay used in methods of remootio.utils
+REMOOTIO_DELAY = 0.5
+
+# Expected minimum Remootio Websocket API version supported by this integration
+EXPECTED_MINIMUM_API_VERSION = 2
+
+# Keys used by the Config flow
+CONF_API_SECRET_KEY = "secret__api_secret_key"
+CONF_API_AUTH_KEY = "secret__api_auth_key"
+CONF_TITLE = "title"
+CONF_SERIAL_NUMBER = "serial_number"
+CONF_DATA = "data"
+
+# Keys for event data fired by remootio.cover.RemootioCoverEventListener
+ATTR_SERIAL_NUMBER = CONF_SERIAL_NUMBER
+
+# Key for the dictionary entry which holds the instance of aioremootio.RemootioClient to connect to the Remootio device using the Remootio Websocket API
+REMOOTIO_CLIENT = "remootio_client"
